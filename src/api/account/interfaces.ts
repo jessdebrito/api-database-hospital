48e13ad01d3809c2a105a02e14b21bbbc003852c
jessdebrito <debrito.herself@gmail.com>
2024-07-18 01:01:56 -0300

    login system: hashing

---
+++ src/api/account/interfaces.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+import { accountCreateSchema, accountSchema } from "./schemas";
+
+
+export type Account = z.infer<typeof accountSchema>
+export type accountCreate = z.infer<typeof accountCreateSchema>
---
+++ src/api/account/schemas.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const accountSchema = z.object({
+    id: z.number().positive().int(),
+    fullName: z.string().max(100),
+    email: z.string().email().max(100),
+    password: z.string().max(255),
+    createdAt: z.date(),
+    updatedAt: z.date(),
+});
+
+export const accountCreateSchema = accountSchema.omit({
+    id: true,
+    createdAt: true,
+    updatedAt: true,
+});

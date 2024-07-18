4d51f0d42aad5b64a5e72b5ffbf39d0c95e6c6a0
jessdebrito <debrito.herself@gmail.com>
2024-07-18 02:31:18 -0300

    Login system: JWT
    Login UNAUTHORIZED

--- src/api/session/service.ts
+++ src/api/session/service.ts
@@ -1,11 +1,16 @@
 import { prisma } from "../../../prisma/prisma.client";
 import { SessionLogin } from "./interfaces";
+import * as bcrypt from "bcryptjs";
 
 export class SessionService {
     public login = async (payload: SessionLogin) => {
         const account = await prisma.account.findUnique({ 
             where: { email: payload.email },
     });
+
+    const passwordMatch = await bcrypt.compare(payload.password, account?.password)
+
+
         return ({ message: "POST /login"});
     };
 }

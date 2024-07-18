597ea2e0e9a1663552ee6b8205bb4b485d3ce484
jessdebrito <debrito.herself@gmail.com>
2024-07-17 23:46:05 -0300

    version routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from  "express";
 import "express-async-errors";
-import { accountRouter } from "./api/account";
+import { initRouters } from "./routes";
 
 export const app = express();
 
 app.use(express.json());
 
-app.use("/accounts", accountRouter)
+initRouters(app)
